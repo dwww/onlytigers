f44a0a39e0d05346fd39691e4e8ef3d521f4a63a
Jan Vatovec <jv1356@student.uni-lj.si>
2013-05-28 23:37:21 +0200

    dodal fileje za login, signup

---
+++ app/views/toolbarSign.html.php
@@ -0,0 +1,35 @@
+								<ul class="nav pull-right">
+									<li><a href="/signup">Sign Up</a></li>
+									<li class="dropdown">
+										<a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
+										<div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
+											<form method="post" action="login" accept-charset="UTF-8">
+												<input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
+												<input style="margin-bottom: 15px;" type="password" placeholder="Password" id="password" name="password">
+												<input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1">
+												<label class="string optional" for="user_remember_me"> Remember me</label>
+												<input class="btn btn-primary btn-block" type="submit" id="sign-in" value="Sign In">
+											</form>
+										</div>
+									</li>
+								</ul>
+								
+								
+								
+								
+	<script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+	&nbsp;<script type="text/javascript">
+								
+<div id="ytCinemaMessage" style="display: none;"></div><script id="hiddenlpsubmitdiv" style="display: none;"></script><script>try{for(var lastpass_iter=0; lastpass_iter < document.forms.length; lastpass_iter++){ var lastpass_f = document.forms[lastpass_iter]; if(typeof(lastpass_f.lpsubmitorig2)=="undefined"){ lastpass_f.lpsubmitorig2 = lastpass_f.submit; lastpass_f.submit = function(){ var form=this; var customEvent = document.createEvent("Event"); customEvent.initEvent("lpCustomEvent", true, true); var d = document.getElementById("hiddenlpsubmitdiv"); for(var i = 0; i < document.forms.length; i++){ if(document.forms[i]==form){ d.innerText=i; } } d.dispatchEvent(customEvent); form.lpsubmitorig2(); } } }}catch(e){}</script>
